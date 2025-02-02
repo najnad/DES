52ca1b9c9b33ce0be49c5e350bd41fadf3dbc68e
najnad <n4jn4d@gmail.com>
2025-02-02 06:23:56 -0800

    added comments

--- DES/KeyScheduler.py
+++ DES/KeyScheduler.py
@@ -2,7 +2,8 @@ from Tables import PC1_TABLE, PC2_TABLE
 from DES import permutation
 
 
-# Implements the PC-2 functions. Permutates 56-bit key into 48-bits.
+# Implements the PC-2 functions. 
+# Permutates 56-bit key into 48-bits.
 # Returns list of 16 [48-bit] keys, permutated using PC2 table.
 def pc2(shifted_keys):
     permuted_keys = []  # Used to store shifted keys
@@ -15,7 +16,8 @@ def pc2(shifted_keys):
     return permuted_keys
 
 
-# Performs left shift on a set of bits. Shift varies (1, 2) depending on the round.
+# Performs left shift on a set of bits. 
+# Shift varies (1, 2) depending on the round.
 def left_shift(bits, shifts):
     return bits[shifts:] + bits[:shifts]
 
@@ -45,9 +47,14 @@ def lcs(permuted_key):
 # Function to create subkeys from the original key.
 # Returns list of keys.
 def create_keys(key):
-    kplus = permutation(PC1_TABLE, key)  # Permutate using PC-1 table
-    shifted_keys = lcs(kplus)  # Generate 16 keys using left circular shift
-    pc2_keys = pc2(shifted_keys)  # 56 -> 48 bits using PC-2
+    # Permutate using PC-1 table
+    kplus = permutation(PC1_TABLE, key)
+
+    # Generate 16 keys using left circular shift
+    shifted_keys = lcs(kplus)
+
+    # 56 -> 48 bits using PC-2
+    pc2_keys = pc2(shifted_keys)
 
     return pc2_keys
 
